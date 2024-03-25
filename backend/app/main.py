310f2a6aee791bf99f997b3457cfc980b1f924a6
Onivoid <skullyfox.yt@gmail.com>
2024-03-25 22:56:27 +0100

    CORS Origin for development

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,25 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from .routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv('../../.env')
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
 app.include_router(router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 register_tortoise(
     app,
